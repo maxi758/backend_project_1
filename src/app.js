2a8cd305565359f36d6cb23124b97560a35db038
maxi758 <maxi758@gmail.com>
2023-10-30 11:31:22 -0300

    add database connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 const express = require('express');
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+mongoose
+  .connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sjtjxec.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("Server running on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+ /* app.listen(5000, () => {
+    console.log("Server running on port 5000");
+    console.log(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sjtjxec.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`);
+  });*/
