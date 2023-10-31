9cf84ac30341c33e30d890a8ac834273e3ae14eb
maxi758 <maxi758@gmail.com>
2023-10-31 09:39:18 -0300

    add delete for category

--- src/controllers/categories-controller.js
+++ src/controllers/categories-controller.js
@@ -7,48 +7,79 @@ const getCategories = async (req, res, next) => {
   try {
     categories = await Category.find();
   } catch (err) {
-    const error = new HttpError('Fetch failed', 500);
-		return next(error);
+    const error = new HttpError("Fetch failed", 500);
+    return next(error);
   }
-	console.log(categories);
-	if(categories.length === 0) {
-		const error = new HttpError('No categories found', 404);
-		return next(error);
-	}
+  console.log(categories);
+  if (categories.length === 0) {
+    const error = new HttpError("No categories found", 404);
+    return next(error);
+  }
   res.status(200).json({
-    categories/*: categories.map((category) =>
+    categories /*: categories.map((category) =>
       category.toObject({ getters: true })
-    ),*/
+    ),*/,
   });
 };
 
 const createCategory = async (req, res, next) => {
-	const {name, description} = req.body;
-	let result;
-	const category = new Category({
-		name,
-		description
-	});
-	try {
-		result = category.save();
-	} catch (err) {
-		const error = new HttpError('Creation failed', 500);
-		return next(error);
+  const { name, description } = req.body;
+  let result;
+  const category = new Category({
+    name,
+    description,
+  });
+  try {
+    result = category.save();
+  } catch (err) {
+    const error = new HttpError("Creation failed", 500);
+    return next(error);
   }
-	res.status(201).json({result});
-}
+  res.status(201).json({ result });
+};
 
 const updateCategory = async (req, res, next) => {
-	const {pid} = req.params;
-	const propertiesToUpdate = req.body
-	console.log(propertiesToUpdate);
-	const updatedCategory = await Category.findByIdAndUpdate(pid, propertiesToUpdate, {new: true});
-	//const updatedCategory = await Category.findOneAndUpdate({_id: pid}, propertiesToUpdate, {new: true});
-	res.json({updatedCategory});
-}
+  const { pid } = req.params;
+  const propertiesToUpdate = req.body;
+  let updatedCategory;
+  console.log(propertiesToUpdate);
+  try {
+    updatedCategory = await Category.findByIdAndUpdate(
+      pid,
+      propertiesToUpdate,
+      { new: true }
+    );
+  } catch (err) {
+    const error = new HttpError("Update failed", 500);
+    return next(error);
+  }
+  if (!updatedCategory) {
+    const error = new HttpError("Category with given id was not found", 404);
+    return next(error);
+  }
+  //const updatedCategory = await Category.findOneAndUpdate({_id: pid}, propertiesToUpdate, {new: true});
+  res.status(200).json({ updatedCategory });
+};
+
+const deleteCategory = async (req, res, next) => {
+	const { pid } = req.params;
+	let deletedCategory;
+	try {
+		deletedCategory = await Category.findByIdAndDelete(pid);
+	} catch (err) {
+		const error = new HttpError("Update failed", 500);
+    return next(error);
+  }
+  if (!deletedCategory) {
+    const error = new HttpError("Category with given id was not found", 404);
+    return next(error);
+  }
+	res.status(200).json({deletedCategory});
+};
 
 module.exports = {
-	getCategories,
-	createCategory,
-	updateCategory
-}
+  getCategories,
+  createCategory,
+  updateCategory,
+	deleteCategory
+};
--- src/routes/categories-routes.js
+++ src/routes/categories-routes.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const {getCategories, createCategory, updateCategory} = require('../controllers/categories-controller');
+const {
+  getCategories,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/categories-controller");
 
 const router = express.Router();
 
-router.get('/', getCategories);
-router.post('/', createCategory);
-router.patch('/:pid', updateCategory);
+router.get("/", getCategories);
+router.post("/", createCategory);
+router.patch("/:pid", updateCategory);
+router.delete("/:pid", deleteCategory);
 
 module.exports = router;
