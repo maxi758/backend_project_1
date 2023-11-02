da8a62e9790ad05bd2fc4f8d27113ef9f7393e40
maxi758 <maxi758@gmail.com>
2023-11-02 03:45:04 -0300

    add validation

--- src/routes/products-routes.js
+++ src/routes/products-routes.js
@@ -1,26 +1,59 @@
 const express = require("express");
-
+const { check, body, query, oneOf } = require("express-validator");
+const { validate } = require("../utils/validator");
 const {
   getProducts,
   getProductById,
   getProductsByCategory,
   createProduct,
   updateProduct,
-  deleteProduct
+  deleteProduct,
 } = require("../controllers/products-controller");
 
 const route = express.Router();
 
-route.get('/', getProducts);
+route.get("/", getProducts);
 
-route.get('/:pid', getProductById);
+route.get("/:pid", getProductById);
 
-route.get('/categories/:cid', getProductsByCategory);
+route.get("/categories/:cid", getProductsByCategory);
 
-route.post('/', createProduct);
+route.post(
+  "/",
+  [
+    check("name").trim().not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("price").isFloat({ min: 0 }),
+    check("stock").isInt({ min: 0 }),
+    check("image").isURL(),
+    check("category").isMongoId(),
+    validate,
+  ],
+  createProduct
+);
 
-route.patch('/:pid', updateProduct);
+route.patch(
+  "/:pid",
+  [
+    oneOf([
+      check("name").trim().not().isEmpty(),
+      check("description").isLength({ min: 5 }),
+      check("price").isFloat({ min: 0 }),
+      check("stock").isInt({ min: 0 }),
+      check("image").isURL(),
+      check("category").isMongoId(),
+    ]),
+    body().custom((value, { req }) => {
+      if (Object.keys(req.body).length === 0) {
+        throw new Error("Request body is empty");
+      }
+      return true;
+    }),
+    validate,
+  ],
+  updateProduct
+);
 
-route.delete('/:pid', deleteProduct);
+route.delete("/:pid", deleteProduct);
 
 module.exports = route;
