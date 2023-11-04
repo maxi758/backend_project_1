8d5222de42a0b11d7bf54ed5f9ed92614392637b
maxi758 <maxi758@gmail.com>
2023-11-04 18:00:45 -0300

    change schema properties

--- src/models/order.js
+++ src/models/order.js
@@ -3,9 +3,19 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const orderSchema = new schema({
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-    products: [{ type: mongoose.Types.ObjectId, required: false, ref: "Product" }], // Child reference, porque no serán tantos los productos en cada compra
-    });
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  products: [
+    {
+      product: {
+        type: mongoose.Types.ObjectId,
+        required: false,
+        ref: "Product",
+      },
+
+      qty: { type: Number, required: true, default: 1 },
+    },
+  ], // Child reference, porque no serán tantos los productos en cada compra
+});
 
 module.exports = mongoose.model("Order", orderSchema);
